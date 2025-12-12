01dc12b2009ca7d0f6c368327714539f0e5d1e30
Harun AKSAYA <harunaksaya@marmara.edu.tr>
2025-12-12 16:27:07 +0300

    feat: Load CSS via link tag
    Moves CSS loading from `index.js` to `index.html` to avoid potential JavaScript import errors. This ensures the stylesheet is applied early in the page load.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { html } from 'htm/react';
 import App from './App.js';
-import './index.css';
+// CSS is now loaded via <link> in index.html
+// import './index.css'; 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error("Could not find root element to mount to");
